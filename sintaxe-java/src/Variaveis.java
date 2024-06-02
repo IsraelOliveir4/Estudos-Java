f7c734e58b11a31a9ccf29992185b1fe43a2f732
Israel Oliveira <israeloliver16.io@gmail.com>
2024-06-02 08:13:55 -0300

    Adicionando arquivo de variaveis e metodos.

---
+++ sintaxe-java/src/Variaveis.java
@@ -0,0 +1,19 @@
+public class Variaveis {
+    public static void main(String[] args) throws Exception {
+
+        ////////     TIPOS DE VARIAVEIS   //
+
+        byte idade = 123;
+		short ano = 2021;
+		int cep = 21070333; // se começar com zero, talvez tenha que ser outro tipo
+		long cpf = 98765432109L; // se começar com zero, talvez tenha que ser outro tipo
+		float pi = 3.14F;
+		double salario = 1275.33;
+
+        //VARIAVEIS CONSTANTES
+
+        final String BR = "BRASIL";
+
+        System.out.println("Hello, World!");
+    }
+}
